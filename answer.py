aea89213dcd36656cb5b6bf4aa7deba3088eaa56
Miner205 <arthurdonnat05@gmail.com>
2023-12-08 17:41:08 +0100

    enfin ça marche nickel.

---
+++ answer.py
@@ -0,0 +1,20 @@
+import utility.data as data
+
+# Functions for generate an answer to a question of the user.
+
+def generate(vector, relevant_doc):
+    """In : vector is the tf-idf vector of the question ;
+    relevant_doc is the relevant document (returned from fct ..) ;
+    Out : answer."""
+    word = highest_tf_idf(vector)
+
+
+def highest_tf_idf(vector):
+
+
+# def refine(answer):
+    """refine the answer (returned from generate)."""
+   # if #verif que le premier chr est une lettre ,;; fct dans data
+   # refine_answer = data.highercase(answer[0]) + answer[1:]
+
+   # if '.' answer[-1:] : #dernier chr
